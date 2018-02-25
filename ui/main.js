d0b52fb9210540eca65273a8dcff9f3ed5606bb3
Somanth <36670926+Somanth@users.noreply.github.com>
2018-02-25 11:56:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,6 +18,6 @@ button.onclick = function () {
         
     };
     
-    request.open('Get', 'http://somnathsaha738.imad.hasura-app.io/counter', true);
+    request.open('GET', 'http://somnathsaha738.imad.hasura-app.io/counter', true);
     request.send(null);
 };
