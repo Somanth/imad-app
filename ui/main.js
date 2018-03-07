7301fc4615fd50f52505db0b3bb30715a199dd11
Somanth <36670926+Somanth@users.noreply.github.com>
2018-03-07 19:44:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,26 @@
-console.log('Loaded!');
-
-// Counter Code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
     
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
             }
         }
         
     };
     
-    request.open('GET', 'http://somnathsaha738.imad.hasura-app.io/counter', true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://somnathsaha738.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-type, application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
