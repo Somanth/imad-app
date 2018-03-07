0bae5a11e82bfdd2ad6faca5d1b5d5e5fbb67dc0
Somanth <36670926+Somanth@users.noreply.github.com>
2018-03-07 19:58:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,6 +21,6 @@ submit.onclick = function () {
     console.log(username);
     console.log(password);
     request.open('POST', 'http://somnathsaha738.imad.hasura-app.io/login', true);
-    request.setRequestHeader('Content-type, application/json');
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
